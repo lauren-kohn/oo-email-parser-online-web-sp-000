51d6b86b5d11b5d146d472c63285ca7e2b1bb76a
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-01 04:12:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,8 +11,7 @@ class EmailAddressParser
   @@emails = []
   
   def initialize(data)
-    @@emails = data.split(",")
-    @@emails = data.split(" ")
+    @@emails = data.split(", ")
   end
   
   def parse
